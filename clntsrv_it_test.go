57ca0322b4fbb8265ab9bf9d18d80f499bfcc09c
DanB <danb@cgrates.org>
2017-04-28 12:33:40 +0200

    Client integration tests for both UDP and TCP

--- clntsrv_it_test.go
+++ clntsrv_it_test.go
@@ -8,10 +8,13 @@ package radigo
 import (
 	"strings"
 	"testing"
+	"time"
+	//"fmt"
 )
 
 var (
-	dict *Dictionary
+	dict    *Dictionary
+	testNet string
 )
 
 func handleAuth(req *Packet) (rpl *Packet, err error) {
@@ -32,47 +35,15 @@ func handleAcct(req *Packet) (rpl *Packet, err error) {
 	return
 }
 
-func TestRadServerStart(t *testing.T) {
-	freeRADIUSDocDictSample := `
-# Most of the lines are copied from freeradius documentation here:
-# http://networkradius.com/doc/3.0.10/concepts/dictionary/introduction.html
-
-# Attributes
-ATTRIBUTE    User-Name    1    string
-ATTRIBUTE    Password     2    string
-
-# Alias values
-VALUE    Framed-Protocol    PPP    1
-
-# Vendors
-VENDOR    Cisco    9
-VENDOR    Microsoft 311
-
-# Vendor AVPs
-BEGIN-VENDOR    Cisco
-ATTRIBUTE       Cisco-AVPair    1   string
-ATTRIBUTE       Cisco-NAS-Port  2	string
-END-VENDOR      Cisco
-`
-	dict = RFC2865Dictionary()
-	// Load some VSA for our tests
-	if err := dict.parseFromReader(strings.NewReader(freeRADIUSDocDictSample)); err != nil {
-		t.Error(err)
-	}
-	go NewServer("tcp", "localhost:1812",
-		map[string]string{"127.0.0.1": "CGRateS.org"},
-		map[string]*Dictionary{"127.0.0.1": RFC2865Dictionary()},
-		map[PacketCode]func(*Packet) (*Packet, error){AccessRequest: handleAuth}).ListenAndServe()
-	go NewServer("tcp", "localhost:1813",
-		map[string]string{"127.0.0.1": "CGRateS.org"},
-		map[string]*Dictionary{"127.0.0.1": RFC2865Dictionary()},
-		map[PacketCode]func(*Packet) (*Packet, error){AccountingRequest: handleAcct}).ListenAndServe()
+var radSTestsIT = []func(t *testing.T){
+	testRadClientAuth,
+	testRadClientAccount,
 }
 
-func TestRadClientAuth(t *testing.T) {
-	authClnt, err := NewClient("tcp", "localhost:1812", "CGRateS.org", dict, 0)
+func testRadClientAuth(t *testing.T) {
+	authClnt, err := NewClient(testNet, "127.0.0.1:1812", "CGRateS.org", dict, 0)
 	if err != nil {
-		t.Error(err)
+		t.Fatal(err)
 	}
 	req := &Packet{
 		Code:       AccessRequest,
@@ -114,7 +85,7 @@ func TestRadClientAuth(t *testing.T) {
 	}
 }
 
-func TestRadClientAccount(t *testing.T) {
+func testRadClientAccount(t *testing.T) {
 	req := &Packet{
 		Code:       AccountingRequest,
 		Identifier: 2,
@@ -133,9 +104,9 @@ func TestRadClientAccount(t *testing.T) {
 			},
 		},
 	}
-	acntClnt, err := NewClient("tcp", "localhost:1813", "CGRateS.org", dict, 0)
+	acntClnt, err := NewClient(testNet, "127.0.0.1:1813", "CGRateS.org", dict, 0)
 	if err != nil {
-		t.Error(err)
+		t.Fatal(err)
 	}
 	reply, err := acntClnt.SendRequest(req)
 	if err != nil {
@@ -146,5 +117,65 @@ func TestRadClientAccount(t *testing.T) {
 	}
 	if len(reply.AVPs) != len(req.AVPs) {
 		t.Errorf("Expecting: %+v, received: %+v", req.AVPs, reply.AVPs)
+	}
+}
+
+func TestRadServerStart(t *testing.T) {
+	freeRADIUSDocDictSample := `
+# Most of the lines are copied from freeradius documentation here:
+# http://networkradius.com/doc/3.0.10/concepts/dictionary/introduction.html
+
+# Attributes
+ATTRIBUTE    User-Name    1    string
+ATTRIBUTE    Password     2    string
+
+# Alias values
+VALUE    Framed-Protocol    PPP    1
+
+# Vendors
+VENDOR    Cisco    9
+VENDOR    Microsoft 311
+
+# Vendor AVPs
+BEGIN-VENDOR    Cisco
+ATTRIBUTE       Cisco-AVPair    1   string
+ATTRIBUTE       Cisco-NAS-Port  2	string
+END-VENDOR      Cisco
+`
+	dict = RFC2865Dictionary()
+	// Load some VSA for our tests
+	if err := dict.parseFromReader(strings.NewReader(freeRADIUSDocDictSample)); err != nil {
+		t.Error(err)
+	}
+	go NewServer("udp", "localhost:1812",
+		map[string]string{"127.0.0.1": "CGRateS.org"},
+		map[string]*Dictionary{"127.0.0.1": RFC2865Dictionary()},
+		map[PacketCode]func(*Packet) (*Packet, error){AccessRequest: handleAuth}).ListenAndServe()
+	go NewServer("udp", "localhost:1813",
+		map[string]string{"127.0.0.1": "CGRateS.org"},
+		map[string]*Dictionary{"127.0.0.1": RFC2865Dictionary()},
+		map[PacketCode]func(*Packet) (*Packet, error){AccountingRequest: handleAcct}).ListenAndServe()
+	go NewServer("tcp", "localhost:1812",
+		map[string]string{"127.0.0.1": "CGRateS.org"},
+		map[string]*Dictionary{"127.0.0.1": RFC2865Dictionary()},
+		map[PacketCode]func(*Packet) (*Packet, error){AccessRequest: handleAuth}).ListenAndServe()
+	go NewServer("tcp", "localhost:1813",
+		map[string]string{"127.0.0.1": "CGRateS.org"},
+		map[string]*Dictionary{"127.0.0.1": RFC2865Dictionary()},
+		map[PacketCode]func(*Packet) (*Packet, error){AccountingRequest: handleAcct}).ListenAndServe()
+	time.Sleep(1 * time.Millisecond)
+}
+
+func TestRadClientUDP(t *testing.T) {
+	testNet = "udp"
+	for _, stest := range radSTestsIT {
+		t.Run("TestRadClientUDP", stest)
+	}
+}
+
+func TestRadClientTCP(t *testing.T) {
+	testNet = "tcp"
+	for _, stest := range radSTestsIT {
+		t.Run("TestRadClientTCP", stest)
 	}
 }
